710970fd92930c5f55e1ee2770812324613788a5
Yusuf Eryilmaz <yusuferyilmaz1819@gmail.com>
2021-01-07 00:23:10 -0500

    poolDjango

---
+++ poll_project/poll/views.py
@@ -0,0 +1,53 @@
+from django.shortcuts import render,redirect
+from django.http import  HttpResponse
+
+from .forms import CreatePollForm
+from .models import Poll
+
+
+def home(request):
+    polls=Poll.objects.all()
+    context={
+        'polls':polls
+    }
+    return render(request,'poll/home.html',context)
+
+def create(request):
+    if request.method=="POST":
+        form=CreatePollForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form =CreatePollForm()
+    context={
+        'form':form
+    }
+    return render(request,'poll/create.html',context)
+
+def vote(request,poll_id):
+    poll=Poll.objects.get(pk=poll_id)
+    if request.method=='POST':
+    #    print(request.POST['poll'])
+        selected_option=request.POST['poll']
+        if selected_option=='option1':
+            poll.option_one_count+=1
+        elif selected_option=='option2':
+            poll.option_two_count+=1
+        elif selected_option=='option3':
+            poll.option_three_count+=1
+        else:
+            return HttpResponse(400,'invalid form')
+        poll.save()
+        return redirect('result',poll_id)
+    context={
+        'poll':poll
+    }
+    return render(request,'poll/vote.html',context)
+
+def result(request,poll_id):
+    poll=Poll.objects.get(pk=poll_id)
+    context={
+        "poll":poll
+    }
+    return render(request,'poll/result.html',context)
